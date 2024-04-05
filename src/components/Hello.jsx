3ce4f8291600c6ee180e6dafa7236742d9c18a0e
Omar Flores <cerberusprogrammer@gmail.com>
2024-04-05 14:31:07 -0700

    Integracion visual y conexion con redes

--- src/components/Hello.jsx
+++ src/components/Hello.jsx
@@ -1,35 +1,33 @@
-
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
 function Hello() {
-    const [data, setData] = useState('Hola');
+  const [data, setData] = useState("Hola");
 
-    useEffect(() => {
-        console.log('Hello1');
-        const fetchData = async () => {
-            try {
-                const response = await fetch('https://randomuser.me/api/');
-                if (response.ok) {
-                    const jsonData = await response.json();
-                    console.log(jsonData);
-                    setData(jsonData);
-                } else {
-                    console.error('Error al obtener los datos');
-                }
-            } catch (error) {
-                console.error('Error en la petición:', error);
-            }
-        };
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("https://randomuser.me/api/");
+        if (response.ok) {
+          const jsonData = await response.json();
+          console.log(jsonData);
+          setData(jsonData);
+        } else {
+          console.error("Error al obtener los datos");
+        }
+      } catch (error) {
+        console.error("Error en la petición:", error);
+      }
+    };
 
-        fetchData();
-    }, []);
+    fetchData();
+  }, []);
 
-    return (
-        <>
-            <p>hola</p>
-            <pre>{JSON.stringify(data)}</pre>
-        </>
-    );
+  return (
+    <>
+      <p>hola</p>
+      <pre>{JSON.stringify(data)}</pre>
+    </>
+  );
 }
 
 export default Hello;
---
+++ src/data/routes.js
@@ -0,0 +1,7 @@
+const routes = [
+  {
+    city: "nombre",
+  },
+];
+
+const casetas = [{}];
